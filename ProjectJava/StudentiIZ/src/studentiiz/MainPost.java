1ef06b3d6575d2796db224ed6ba54b0df812e7f2
savaava <andreasavastano4388@gmail.com>
2024-11-29 12:29:35 +0100

    letio thread2

--- ProjectJava/StudentiIZ/src/studentiiz/MainPost.java
+++ ProjectJava/StudentiIZ/src/studentiiz/MainPost.java
@@ -102,6 +102,47 @@ public class MainPost {
         
         /* 29_11
         un thread aspetta che l'altro produce il dato: produttore consumatore
-        */
+        in questo caso l'incapsulamento è ancora più importante perchè qualsiasi thread potrebbe accedere 
+        agli attributi non incapsulati
+        
+        problematica dell'attesa:
+        fino a mo thread interagiscono sulla risorsa senza tenere conto dello stato della risorsa
+        intercettare quando un thread ha terminato la sua esecuzione, una notifica di quando la risorsa è
+        stata modificata 
+        sleep -> il thread non è più attivo è sospeso e il processore può usare le proprie risorse 
+        per altre istruzioni però non rilascia il mutex se dovesse averlo acquisito prima!!
+        in questo caso l'attesa è fuori dalle operazioni che prevedono l'acquisizione del mutex, così è libera
+        la risorsa.
+        attesa attiva (sempre sconsiglabile) perchè il processore sta comunque controllando 
+        la condizione e si dovrebbe quindi sospendere l'esecuzione e ricevere una notifica di quando può continuare
+        
+        metodo join per intercettare il completamento dell'esecuzione di un thread, il thread principale 
+        quando vede join -> aspetta che il thread su cui è stato invocato il metodo join finisca
+        rispetto al thread principale la join diventa bloccante, quindi dobbiamo trovare una condizione di 
+        terminazione del thread
+        allora join può servire perchè vogliamo aspettare prima che un thread faccia le sue operazioni e poi il
+        thread principale continua
+        
+        buffer risorsa condivisa e i thread che hanno lavori differenti produzione e consumazione
+        1 o più thread che consumano e prelevano 
+        accedo alla risorsa, vedo se il dato è disponibile, se non  è allora rilascio il mutex mi serve quindi
+        un meccanismo che mi risvegli quando è pronta la risorsa che è aggiornata o aggiunta
+        2 meccanismi: 
+        1 per sospendere l'esecuzione e rilasciare
+        2 per risvegliarmi 
+        il thread verifica la condizione e se non rispetta le sue esigenze allora si addormenta e viene risvegliato
+        quando quella condizione è verificata (no attesa attiva)
+        metodi: wait e notifyAll
+        wait: thread sospende la propria esecuzione e rilascia il mutex che aveva acquisito, fino a quando un 
+        altro thread invoca notifyAll per sbloccare il thread che era in wait e riceve questa notifica e 
+        controlla la condizione e poi acquisisce il mutex.
+        notifyAll -> risveglia tutti i thread che erano in attesa (con wait) su quella risorsa (obj) e uno 
+        dei thread acquisisce il mutex (il primo) 
+        anche qui si può gestire dal lato dei thread o dal lato della risorsa
+        la condizione deve essere fatta in un ciclo perchè il thread ottiene il mutex perchè un altro thread ha
+        invocato notifyAll ma deve verificare che può effettivamente continuare, e quindi deve ricontrollare 
+        la condizione e se non è soddisfatta allora invoca di nuovo la wait e rilascia il mutex
+        */        
     }
 }
+ 
